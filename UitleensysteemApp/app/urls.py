65ad06bcaf93e7981021ed803fc7417f23df2b97
Kevin Poelman <56067721+Neo-Phoenix@users.noreply.github.com>
2024-05-12 01:25:44 +0200

    Django Admin, alle app.models toegevoegd in admin.py voor het admin paneel

--- UitleensysteemApp/app/urls.py
+++ UitleensysteemApp/app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.signin, name="signin"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
+    path('overview', views.login, name="signin"),
 ]
--- UitleensysteemApp/app/views.py
+++ UitleensysteemApp/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -8,3 +9,16 @@ def signin(request):
 
 def signup(request):
     return render(request, 'app/signup.html')
+
+def login(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        
+    else:
+        return render(request, 'app/signin.html')
+
+def logout(request):
+    logout(request)
